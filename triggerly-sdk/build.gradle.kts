d60d370751bc938b8c2b71793fcbf8f69567712e
SEUNGHO1216 <seungho1216@naver.com>
2026-09-05 13:52:37 +0900

    feat(sdk): triggerly-sdk를 GitHub Packages로 publish하는 설정 추가
    외부 프로젝트에서 의존성 좌표로 끌어다 쓸 수 있도록 maven-publish
    플러그인과 GitHubPackages 리포지토리를 등록. URL의 GITHUB_USERNAME은
    실제 원격 저장소 계정이 정해지면 교체 필요.

--- triggerly-sdk/build.gradle.kts
+++ triggerly-sdk/build.gradle.kts
@@ -1,4 +1,30 @@
+plugins {
+	id("maven-publish")
+}
+
 dependencies {
 	implementation(libs.jackson.module.kotlin)
 	testImplementation(libs.kotest.assertions.core)
+}
+
+publishing {
+	publications {
+		create<MavenPublication>("maven") {
+			from(components["java"])
+			groupId = project.group.toString()
+			artifactId = "triggerly-sdk"
+			version = project.version.toString()
+		}
+	}
+	repositories {
+		maven {
+			name = "GitHubPackages"
+			// TODO: YOUR_GITHUB_USERNAME 을 실제 GitHub 계정/조직명으로 교체
+			url = uri("https://maven.pkg.github.com/YOUR_GITHUB_USERNAME/triggerly-claude")
+			credentials {
+				username = System.getenv("GITHUB_ACTOR") ?: project.findProperty("gpr.user") as String?
+				password = System.getenv("GITHUB_TOKEN") ?: project.findProperty("gpr.token") as String?
+			}
+		}
+	}
 }
